fbfeba052c482b2e3115ed902852b32720cef7d1
Leleadelante <leleadelante@gmail.com>
2026-03-21 02:54:16 -0300

    Create app/page.js with AbogadoChat component

---
+++ app/page.js
@@ -0,0 +1,8 @@
+import AbogadoChat from './components/AbogadoChat';
+export const metadata = {
+  title: 'Abogado Online IA - Argentina',
+  description: 'Consultas legales con inteligencia artificial basada en leyes argentinas',
+};
+export default function Home() {
+  return <AbogadoChat />
+}
